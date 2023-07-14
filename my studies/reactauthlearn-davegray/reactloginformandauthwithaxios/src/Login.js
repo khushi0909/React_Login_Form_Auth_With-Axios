7ad96b9c92a0b27742932290a79f0fe34db5455c
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-07-14 12:02:58 +0200

    jsx for form and defining handleSUbmit in progress

--- my studies/reactauthlearn-davegray/reactloginformandauthwithaxios/src/Login.js
+++ my studies/reactauthlearn-davegray/reactloginformandauthwithaxios/src/Login.js
@@ -19,13 +19,51 @@ const Login = () => {
         setErrMsg('');
     }, [user, pwd])
 
+    const handleSubmit = async(e) => {
+
+
+        e.preventDefault();
+    }
+
 
   return (
     <section>
 
         <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+
+        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}            //putting user value and making it controlled input ,this is imp when we want to clear this form and you will definately do this 
+                            required
+                        />
+        <label htmlFor="passwrod">Passwrod:</label>
+                        <input
+                            type="password"
+                            id="passwrod"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}            //putting user value and making it controlled input ,this is imp when we want to clear this form and you will definately do this 
+                            required
+                        />
+                        <button>Sign In</button>
+        
 
 
+            </form>
+
+            <p>
+                        Need an Account?<br />
+                        <span className="line">
+                            {/*put router link here*/}
+                            <a href="#">Sign Up</a>
+                        </span>
+                    </p>
     </section>
   )
 }
