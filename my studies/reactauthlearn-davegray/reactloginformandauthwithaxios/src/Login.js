a558536a4cefbd51695df152c67c6b9d5348d660
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-07-14 13:06:12 +0200

    completed form by adding some functionalities that will connect with backend server link to check username and pwd and some error messages handling

--- my studies/reactauthlearn-davegray/reactloginformandauthwithaxios/src/Login.js
+++ my studies/reactauthlearn-davegray/reactloginformandauthwithaxios/src/Login.js
@@ -1,92 +1,119 @@
-import React from 'react'
-import {useRef , useState ,useEffect,useContext} from 'react'
-import AuthContext from './api/context/AuthProvider';
+import React from "react";
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "./context/AuthProvider";
+import axios from "./api/axios";
+
+const LOGIN_URL = "./auth"; ///This should match with the backend
 
 const Login = () => {
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const {setAuth} =  useContext(AuthContext)
-    const userRef = useRef();
-    const errRef = useRef();
-    
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
-    const [success, setSuccess] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
 
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-            console.log(user,pwd);
-            setUser('');
-            setPwd('');
-            setSuccess(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+//axios is great because it theows an error if it gets it ,we dont have to cehck here  like we do with fetch to see if the response was ok and likewise also we dont have to take the response and convert it to JSON
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd },{
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        })
+      );
+      console.log(JSON.stringify(response?.data));
+      console.log(JSON.stringify(response));
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
+    } catch (err) {
 
-
+        if (!err?.response) {
+            setErrMsg('No Server Response');
+        } else if (err.response?.status === 400) {
+            setErrMsg('Missing Username or Password');
+        } else if (err.response?.status === 401) {
+            setErrMsg('Unauthorized');
+        } else {
+            setErrMsg('Login Failed');
+        }
+        errRef.current.focus();
     }
 
+    console.log(user, pwd);
+    setUser("");
+    setPwd("");
+    setSuccess(true);
+  };
 
   return (
-
     <>
-    {success ? (
+      {success ? (
         <section>
-            <h1>You are logged in!</h1>
-            <br />
-            <p>
-                <a href="#">Go to Home</a>
-            </p>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
         </section>
-    ) : (
-    <section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user} //putting user value and making it controlled input ,this is imp when we want to clear this form and you will definately do this
+              required
+            />
+            <label htmlFor="passwrod">Passwrod:</label>
+            <input
+              type="password"
+              id="passwrod"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd} //putting user value and making it controlled input ,this is imp when we want to clear this form and you will definately do this
+              required
+            />
+            <button>Sign In</button>
+          </form>
 
-        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-        <h1>Sign In</h1>
-        <form onSubmit={handleSubmit}>
-
-        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}            //putting user value and making it controlled input ,this is imp when we want to clear this form and you will definately do this 
-                            required
-                        />
-        <label htmlFor="passwrod">Passwrod:</label>
-                        <input
-                            type="password"
-                            id="passwrod"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}            //putting user value and making it controlled input ,this is imp when we want to clear this form and you will definately do this 
-                            required
-                        />
-                        <button>Sign In</button>
-        
-
-
-            </form>
-
-            <p>
-                        Need an Account?<br />
-                        <span className="line">
-                            {/*put router link here*/}
-                            <a href="#">Sign Up</a>
-                        </span>
-                    </p>
-    </section>
-    )}
+          <p>
+            Need an Account?
+            <br />
+            <span className="line">
+              {/*put router link here*/}
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
     </>
-  )
-}
+  );
+};
 
-
-export default Login
+export default Login;
